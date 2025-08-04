3998fe16e2073e2984e34c4c71b97a715eb29f3a
vipbroken <hiv968676@gmail.com>
2025-08-04 19:25:30 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "bARmWY6B#t314N7sB0JG3SfIY1_sbhW3E3Cj-GpW_hMAzBEyapS0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
